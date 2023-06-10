03cd0dd89325bb0e295ebb442d678af4f357fb1d
Brandon Nolin <brandonolinn@gmail.com>
2023-06-10 02:19:56 -0500

    created functions for piece movements. pawns now move properly

--- src/Components/Chessboard/Chessboard.js
+++ src/Components/Chessboard/Chessboard.js
@@ -1,9 +1,72 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Square from "./Square";
 import { initialBoard } from "./initial-board-state";
 
 function ChessBoard() {
   const [board, setBoard] = useState(initialBoard);
+  const [selectedPiece, setSelectedPiece] = useState([]);
+
+  useEffect(() => {
+    // pieceClicked(12, 1);
+  }, []);
+
+  const pieceClicked = (selectedPiece, legalMoves) => {
+    setBoard((prevBoard) => {
+      let newBoard = [...prevBoard];
+
+      //unselect all other board squares and unset as legal moves.
+      newBoard.forEach((row) => {
+        row.forEach((square) => {
+          square.selected = false;
+          square.legalMove = false;
+        });
+      });
+
+      //select piece
+      newBoard[selectedPiece[0]][selectedPiece[1]].selected = true;
+      setSelectedPiece([
+        selectedPiece[0],
+        selectedPiece[1],
+        newBoard[selectedPiece[0]][selectedPiece[1]].piece,
+      ]);
+
+      legalMoves.forEach((legalMove) => {
+        if (newBoard[legalMove[0]][legalMove[1]].piece === null) {
+          newBoard[legalMove[0]][legalMove[1]].legalMove = true;
+        }
+      });
+
+      return newBoard;
+    });
+  };
+
+  const movePiece = (newLocation) => {
+    setBoard((prevBoard) => {
+      let newBoard = [...prevBoard];
+
+      //unselect all other board squares and unset as legal moves.
+      newBoard.forEach((row) => {
+        row.forEach((square) => {
+          square.selected = false;
+          square.legalMove = false;
+        });
+      });
+
+      //find selected piece
+
+      newBoard[newLocation[0]][newLocation[1]].piece = selectedPiece[2];
+
+      if (
+        newBoard[selectedPiece[0]][selectedPiece[1]].piece?.includes("pawn")
+      ) {
+        newBoard[newLocation[0]][newLocation[1]].pawnHasMoved = true;
+      }
+
+      newBoard[selectedPiece[0]][selectedPiece[1]].piece = null;
+
+      return newBoard;
+    });
+  };
 
   const renderBoard = () => {
     let squares = [];
@@ -12,7 +75,14 @@ function ChessBoard() {
       row.forEach((square) => {
         squares.push(
           <Square
+            key={square.rowId + "-" + square.columnId}
             piece={square.piece}
+            pieceClicked={pieceClicked}
+            movePiece={movePiece}
+            columnId={square.columnId}
+            legalMove={square.legalMove}
+            rowId={square.rowId}
+            pawnHasMoved={square.pawnHasMoved}
             classes={`square ${square.color} ${square.legalMove && "movable"} ${
               square.selected
             }`}
--- src/Components/Chessboard/Square.js
+++ src/Components/Chessboard/Square.js
@@ -1,5 +1,50 @@
+import WhiteBishop from "../Pieces/WhiteBishop";
+import WhiteRook from "../Pieces/WhiteRook";
+import WhiteKnight from "../Pieces/WhiteKnight";
+import WhitePawn from "../Pieces/WhitePawn";
+
 function Square(props) {
-  return <div className={props.classes}></div>;
+  function squareClicked() {
+    if (props.legalMove) {
+      props.movePiece([props.rowId, props.columnId]);
+    } else {
+      // props.onSquareClicked();
+    }
+  }
+
+  return (
+    <div onClick={squareClicked} className={props.classes}>
+      {props.piece === "white-pawn" && (
+        <WhitePawn
+          pieceClicked={props.pieceClicked}
+          columnId={props.columnId}
+          rowId={props.rowId}
+          pawnHasMoved={props.pawnHasMoved}
+        />
+      )}
+      {props.piece === "white-rook" && (
+        <WhiteRook
+          pieceClicked={props.pieceClicked}
+          columnId={props.columnId}
+          rowId={props.rowId}
+        />
+      )}
+      {props.piece === "white-knight" && (
+        <WhiteKnight
+          pieceClicked={props.pieceClicked}
+          columnId={props.columnId}
+          rowId={props.rowId}
+        />
+      )}
+      {props.piece === "white-bishop" && (
+        <WhiteBishop
+          pieceClicked={props.pieceClicked}
+          columnId={props.columnId}
+          rowId={props.rowId}
+        />
+      )}
+    </div>
+  );
 }
 
 export default Square;
