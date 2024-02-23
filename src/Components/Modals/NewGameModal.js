8a7abb3b9426432ded90379edc121bdcad129291
Brandon Nolin <brandonolinn@gmail.com>
2024-02-23 09:30:11 -0600

    Makes new game/end game modal responsive
    Added styles for the new game and end game modals so that they now look
    fine on smaller screens.

--- src/Components/Modals/NewGameModal.js
+++ src/Components/Modals/NewGameModal.js
@@ -30,29 +30,29 @@ function NewGameModal({ setPregameState, setIsCreator, setRoomCode, connectSocke
 
   return (
     <div className="absolute flex justify-center items-center w-full h-screen bg-zinc-950/90 z-10">
-      <div className="w-1/3 shadow-lg top-0 opacity-100">
-        <div className="bg-zinc-600 flex justify-center items-center rounded-t-lg">
+      <div className="w-full lg:w-1/3 md:w-2/3 shadow-lg top-0 opacity-100">
+        <div className="bg-zinc-600 flex justify-center items-center rounded-t-none md:rounded-t-lg">
           <span className="p-6 text-center text-5xl font-medium text-white">Play a Game</span>
         </div>
-        <div className="bg-zinc-800 rounded-b-lg">
-          <div className="flex justify-between py-10 px-6">
+        <div className="bg-zinc-800 rounded-b-none md:rounded-b-lg">
+          <div className="flex justify-between gap-4 py-10 px-6">
             <button
               type="button"
-              class="rounded-md bg-blue-700 px-8 py-2.5 text-2xl font-semibold text-gray-200 shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              class="rounded-md bg-blue-700 px-8 py-2.5 text-2xl md:text-4xl lg:text-2xl font-semibold text-gray-200 shadow-sm hover:bg-blue-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Find Game
             </button>
             <button
               onClick={createGame}
               type="button"
-              class="rounded-md bg-zinc-800 px-8 py-2.5 text-2xl font-semibold text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-zinc-700"
+              class="rounded-md bg-zinc-800 px-8 py-2.5 text-2xl md:text-4xl lg:text-2xl font-semibold text-gray-200 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-zinc-700"
             >
               Play a Friend
             </button>
           </div>
-          <div className={`p-6 ${roomCode ? "block" : "hidden"}`}>
+          <div className={`p-6 pt-0 ${roomCode ? "block" : "hidden"}`}>
             <div className="p-2 px-4 flex items-center justify-between rounded-md bg-zinc-500">
-              <p className="text-gray-200">
+              <p className="text-gray-200 text-xl lg:text-base">
                 {window.location.href}?roomCode={roomCode}
               </p>
               <div className="relative">
